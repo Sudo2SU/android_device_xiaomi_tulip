a8e2dd9bf05a49923b5ef0d6eb81a7b4c546a114
Bruno Martins <bgcngm@gmail.com>
2018-02-07 00:41:09 +0000

    tulip: Use MIUI poweroff charging animation
     * It's cool, and more importantly... it actually works!
       Oh look, it even displays battery level. Wow. :-P
    
    Change-Id: I6351e2eac6df89aa1b6b98e4e6b2815fa5c37794

--- device.mk
+++ device.mk
@@ -240,6 +240,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service
 
+PRODUCT_PACKAGES += \
+    chargeonlymode
+
 # Hide Notch
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
