b4a36d9e15bcb319b50026175f04fabd00d2567c
Ryan Andri <ryan-andri@outlook.com>
2020-01-04 14:44:13 +0700

    tulip: add properties from miui and cleanup

--- properties.mk
+++ properties.mk
@@ -49,22 +49,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.qualcomm.qti.qmmi \
-    persist.vendor.bokeh.switch.lux=290 \
-    persist.vendor.camera.HAL3.enabled=1 \
-    persist.vendor.camera.depth.focus.cb=0 \
-    persist.vendor.camera.exif.make=Xiaomi \
-    persist.vendor.camera.expose.aux=1 \
-    persist.vendor.camera.imglib.usefdlite=1 \
-    persist.vendor.camera.isp.clock.optmz=0 \
-    persist.vendor.camera.isp.turbo=1 \
-    persist.vendor.camera.linkpreview=0 \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.qualcomm.qti.qmmi \
     persist.vendor.camera.preview.ubwc=0 \
     persist.vendor.camera.stats.test=0 \
-    persist.vendor.imx376_ofilm.light.lux=275 \
-    persist.vendor.imx376_ofilm.low.lux=290 \
-    persist.vendor.imx376_sunny.light.lux=275 \
-    persist.vendor.imx376_sunny.low.lux=290 \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.qualcomm.qti.qmmi
+    persist.vendor.camera.depth.focus.cb=0 \
+    persist.vendor.camera.isp.clock.optmz=0 \
+    persist.vendor.camera.linkpreview=0 \
+    persist.vendor.camera.isp.turbo=1 \
+    persist.vendor.camera.exif.make=Xiaomi \
+    persist.vendor.camera.expose.aux=1 \
+    persist.vendor.camera.HAL3.enabled=1 \
+    persist.vendor.camera.ltm.overlap=13
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -73,6 +68,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
     persist.debug.wfd.enable=1 \
     persist.hwc.enable_vds=1 \
+    persist.sys.wfd.virtual=0 \
     ro.opengles.version=196610 \
     ro.qualcomm.cabl=0 \
     ro.sf.lcd_density=440 \
@@ -83,6 +79,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.overlay.izat.optin=rro
+
+# HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=2048
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -102,9 +112,25 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vidc.enc.dcvs.extra-buff-count=2 \
     vidc.enc.target_support_bframe=1
 
+# Memory
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sys.fw.bservice_enable=true
+
 # Netflix custom property
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.netflix.bsp_rev=Q660-13149-1
+
+# Nitz
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.nitz_plmn="" \
+    persist.vendor.radio.nitz_lons_0="" \
+    persist.vendor.radio.nitz_lons_1="" \
+    persist.vendor.radio.nitz_lons_2="" \
+    persist.vendor.radio.nitz_lons_3="" \
+    persist.vendor.radio.nitz_sons_0="" \
+    persist.vendor.radio.nitz_sons_1="" \
+    persist.vendor.radio.nitz_sons_2="" \
+    persist.vendor.radio.nitz_sons_3=""
 
 # QCOM
 PRODUCT_PROPERTY_OVERRIDES += \
